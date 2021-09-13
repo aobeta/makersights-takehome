ed27d1eacae708b0de7b0431762aaf3afd5c380c
Anthony Obeta <choobey21@gmail.com>
2021-09-13 12:49:27 -0600

    create db models

---
+++ db-schema.js
@@ -0,0 +1,23 @@
+/**
+ * Database Model For Products and Brands
+ *
+ * Database Engine : MySQL
+ *
+ * Models using JSON Schema:
+ *  Brand
+ *      - id : int (auto generated and auto incremented)
+ *      - name : string
+ *      - createdAt : Date
+ *      - updatedAt : Date
+ *
+ *  Product
+ *      - id : number (auto-generated and auto incremented)
+ *      - name : string
+ *      - productImageUrl: string
+ *      - price : number
+ *      - createdAt: Date
+ *      - updateAt: Date
+ *      - parentId: int (foreign-key reference to Product Table)
+ *      - brandId: int (foreign-key reference to a Brand Table)
+ *
+ */
